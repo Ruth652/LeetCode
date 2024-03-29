7c0990a5b257238511257683166507df486197d5
Ruth652 <146667677+Ruth652@users.noreply.github.com>
2024-03-29 18:54:32 +0300

    Time: 112 ms (81.84%), Space: 13.1 MB (12.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,19 +1,15 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        for i in nums:
-            if i==0:
-                nums.remove(i)
-                nums.append(i)
-    """
-    cclass Solution(object):
-    def moveZeroes(self, nums):
-       
-        for i in range(len(nums)):
-            if nums[i]==0
-            nums[i+1],nums[i]=nums[i],nums[i+1]
-            
-"""
+  
+class Solution(object): 
+    def moveZeroes(self, nums): 
+        """ 
+        :type nums: List[int] 
+        :rtype: None Do not return anything, modify nums in-place instead. 
+        """ 
+ 
+        left=0 
+        for right in range (len(nums)): 
+            if nums[right]: 
+                nums[left],nums[right]=nums[right],nums[left] 
+                left+=1 
+ 
+        return nums
