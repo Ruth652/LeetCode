22768df7136ab01add7355b5242f77470513fde6
Ruth652 <146667677+Ruth652@users.noreply.github.com>
2024-11-15 21:23:30 +0300

    Time: 7 ms (10.04%), Space: 11.4 MB (88.39%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        swap=False
+        for i in range(len(nums)):
+            for j in range(len(nums)-1-i):
+                 if(nums[j]>nums[j+1]):
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+                    swap=True
+            if(swap==False):
+                break
+        
